21e53468826e2aedf8be951d4aa5763804ef9643
Nash Vador <nashvador@gmail.com>
2022-02-07 17:47:51 -0600

    Answers for 03_only_vowels

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/03_only_vowels.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/03_only_vowels.rb
@@ -1,6 +1,23 @@
 # Write a method, `only_vowels?(str)`, that accepts a string as an arg.
 # The method should return true if the string contains only vowels.
 # The method should return false otherwise.
+def only_vowels?(str)
+consonent = "bcdfghjklmnpqrstvxz"
+consontent_bool = true
+str.each_char do |char|
+    consonent.each_char do |consts|
+        if char == consts
+            consontent_bool = false
+        end
+    end
+end
+if consontent_bool == true
+    return true
+else
+    return false
+end
+end
+
 
 
 p only_vowels?("aaoeee")  # => true
