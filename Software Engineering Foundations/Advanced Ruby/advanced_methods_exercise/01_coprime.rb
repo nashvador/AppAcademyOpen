5679956707816d036f50a7abfaff0caf71a60501
Nash Vador <nashvador@gmail.com>
2022-02-07 17:09:01 -0600

    Ruby exercises

---
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/01_coprime.rb
@@ -0,0 +1,20 @@
+# Write a method, coprime?(num_1, num_2), that accepts two numbers as args.
+# The method should return true if the only common divisor between the two numbers is 1.
+# The method should return false otherwise. For example coprime?(25, 12) is true because
+# 1 is the only number that divides both 25 and 12.
+
+puts "hi"
+def coprime(number1, number2)
+   (2..number2).each do |val|
+        if number1%val == 0 && number2%val == 0
+            return false
+        end
+     
+end
+
+
+
+p coprime?(25, 12)    # => true
+p coprime?(7, 11)     # => true
+p coprime?(30, 9)     # => false
+p coprime?(6, 24)     # => false
