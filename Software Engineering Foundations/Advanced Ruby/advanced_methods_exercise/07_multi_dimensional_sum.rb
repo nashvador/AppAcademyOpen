2c13b250732f22f7831de4bddbdd14902bc098cd
Nash Vador <nashvador@gmail.com>
2022-02-07 18:16:43 -0600

    07 exercise answers

--- Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/07_multi_dimensional_sum.rb
+++ Software Engineering Foundations/Advanced Ruby/advanced_methods_exercise/07_multi_dimensional_sum.rb
@@ -1,6 +1,14 @@
 # Write a method, multi_dimensional_sum(array), that accepts a multidimensional array as an arg 
 # and returns the sum of all numbers in the array.
 
+def multi_dimensional_sum(arr)
+new_array = arr.flatten!
+counter = 0
+new_array.each do |x|
+    counter += x
+end
+return counter
+end
 
 arr_1 = [
     [4, 3, 1],
