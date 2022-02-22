b2c2d69411e9333a4b5b99660b07307c5e33217d
Nash Vador <nashvador@gmail.com>
2022-02-22 13:52:10 -0600

    Completed solution of hangman

--- Software Engineering Foundations/hangman_project 2/lib/hangman.rb
+++ Software Engineering Foundations/hangman_project 2/lib/hangman.rb
@@ -34,7 +34,7 @@ def already_attempted?(char)
 end
 
 def get_matching_indices(character)
-i = 0 
+i = -1
 match = []
 while i = @secret_word.index(character, i+1)
   match << i
@@ -48,5 +48,55 @@ array.each do |idx|
   @guess_word.insert(idx, char)
 end
 end
+
+def try_guess(char)
+if already_attempted?(char)
+  print "that has already been attempted"
+  return false
+else
+  if @secret_word.include?(char)
+    fill_indices(char, get_matching_indices(char))
+  else
+    @remaining_incorrect_guesses -= 1
+  end
+  @attempted_chars << char
+  print @attempted_chars
+  return true
+end
+end
+
+def ask_user_for_guess
+print "Enter a char:"
+guess = gets.chomp
+try_guess(guess)
+end
+
+def win?
+  if @secret_word == @guess_word.join("")
+    print 'WIN'
+    return true
+  else
+    return false
+  end
+end
+
+def lose?
+  if @remaining_incorrect_guesses == 0
+    print "LOSE"
+    return true
+  else
+    return false
+  end
+end
+
+def game_over?
+  if win? == true || lose? == true
+    print @secret_word
+    return true
+  else
+    return
+  end
+end
+
 
 end
