7c50247b3179fe245c934daa7a294fe1ed46e6ca
Nash Vador <nashvador@gmail.com>
2022-02-21 21:07:54 -0600

    Hangman part 1 solutions

--- Software Engineering Foundations/hangman_project 2/lib/hangman.rb
+++ Software Engineering Foundations/hangman_project 2/lib/hangman.rb
@@ -1,4 +1,52 @@
 class Hangman
-  DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+def self.random_word 
+  DICTIONARY.sample
+end
+
+def initialize
+  @secret_word = Hangman.random_word
+  @guess_word = Array.new(@secret_word.length, "_")
+  @attempted_chars = []
+  @remaining_incorrect_guesses = 5
+end
+
+def guess_word
+  @guess_word
+end
+
+def attempted_chars
+  @attempted_chars
+end
+
+def remaining_incorrect_guesses
+  @remaining_incorrect_guesses
+end
+
+def already_attempted?(char)
+  if @attempted_chars.include?(char)
+    return true
+  else
+    return false
+  end
+end
+
+def get_matching_indices(character)
+i = 0 
+match = []
+while i = @secret_word.index(character, i+1)
+  match << i
+end
+match
+end
+
+def fill_indices(char, array)
+array.each do |idx|
+  @guess_word.delete_at(idx)
+  @guess_word.insert(idx, char)
+end
+end
 
 end
