481c1ca29e107cfdcfdc4887413a32de04793472
Nash Vador <nashvador@gmail.com>
2022-02-25 13:20:51 -0600

    Part 1 solutions hotel project

--- Software Engineering Foundations/hotel_project/lib/room.rb
+++ Software Engineering Foundations/hotel_project/lib/room.rb
@@ -1,3 +1,36 @@
 class Room
-  
+  def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+  end
+
+  def capacity
+    @capacity
+  end
+
+  def occupants
+    @occupants
+  end
+
+  def full?
+    if @occupants.length == @capacity
+        return true
+    else
+        return false
+    end
+    end
+
+    def available_space
+        return @capacity - @occupants.length
+    end
+
+    def add_occupant(name)
+        if full?
+            return false
+        else
+            @occupants << name
+            return true
+        end
+    end
+
 end
