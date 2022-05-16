fc5d803c1ff6c53885ec56de067be33f30ffef4d
Nash Vador <nashvador@gmail.com>
2022-05-16 13:03:34 -0500

    no consecutive repeats solution

--- Software Engineering Foundations/recap_exercise_3/recap_3.rb
+++ Software Engineering Foundations/recap_exercise_3/recap_3.rb
@@ -2,3 +2,19 @@ def no_dupes?(arr)
 print arr.reject{|x| arr.count(x)>1}
 end
 
+def no_consecutive_repeats?(arr)
+repeat_bool = true
+arr.each_with_index do |value,idx|
+    if value == arr[idx+1]
+        repeat_bool = false
+        break
+    else
+        repeat_bool = true
+    end
+end
+if repeat_bool == true
+    print true
+else
+    print false
+end
+end
